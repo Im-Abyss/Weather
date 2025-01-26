5c5bfb72eedacfaccb3a7843c56d0a1abca444d5
«Im-Abyss» <enty3811@gmail.com>
2025-01-26 17:42:30 +0300

    Добавил смену города и описание погоды

--- app/handlers.py
+++ app/handlers.py
@@ -1,11 +1,12 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
-import requests  # Импортируем requests для выполнения HTTP-запросов
+import requests
 
 from config import API_key
 from .FSM import Weather
+from .keyboards import change_city
 
 router = Router()
 
@@ -15,21 +16,27 @@ async def start(message: Message, state: FSMContext):
                                caption='Здравствуйте! ✋\nКакой город будем смотреть? 😊')
     await state.set_state(Weather.city)
 
+@router.callback_query(F.data == 'change_city')
+async def new_city(callback: CallbackQuery, state: FSMContext):
+    await callback.answer('Другой город')
+    await callback.message.edit_text('Укажите другой город, пожалуйста 🤔')
+    await state.set_state(Weather.city)
+
 @router.message(Weather.city)
 async def weather(message: Message, state: FSMContext):
     city_name = message.text
-    response_k = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units=metric'
-
+    response_k = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units=metric&lang=ru'
     try:
         response = requests.get(response_k)
         data = response.json()
-
         if response.status_code == 200:
             temperature = data['main']['temp']
-            await message.answer(f'Погода в городе {city_name} сейчас {temperature}°C')
+            weather_description = data['weather'][0]['description']
+            await message.answer(f'Погода в городе {city_name} сейчас {temperature}°C, {weather_description}.', reply_markup=change_city)
         else:
             await message.answer(f'Не удалось получить данные о погоде для города {city_name}. Попробуйте другой город.')
     except Exception as e:
         await message.answer('Произошла ошибка при запросе данных о погоде. Пожалуйста, попробуйте позже.')
-
+        await new_city(message, state)
+    
     await state.clear()
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,1 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+change_city = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Другой город", 
+                              callback_data='change_city')]]
+)
